3f4381124cb202cfcf638a1f101ba1438754a250
Werner Strydom <werners@bloudraak.com>
2013-02-27 21:34:43 -0800

    Created basic website with a home controller

---
+++ src/Sample.Web.Application.Tests/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sample.Web.Application.Controllers;
+
+namespace Sample.Web.Application.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var target = new HomeController();
+
+            // Act
+            ActionResult result = target.Index();
+
+            // Assert
+            Assert.IsNotNull(result, "The HomeController.Index returned null");
+        }
+    }
+}
